909ebc842f732baac4871d8a0cce55bffdb8f7a9
Samuel Schepp <samuel.schepp@mni.thm.de>
2018-06-26 17:24:46 +0200

    implemented banking example

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -1,24 +1,36 @@
 import com.apple.foundationdb.Database;
 import com.apple.foundationdb.FDB;
-import com.apple.foundationdb.tuple.Tuple;
+import java.math.BigInteger;
 
 public class Example {
     public static void main(String[] args) {
         FDB fdb = FDB.selectAPIVersion(510);
 
         try(Database db = fdb.open()) {
-
             db.run(transaction -> {
-                transaction.set("my_key".getBytes(), "Hello, World!".getBytes());
+                transaction.set("konto_A".getBytes(), BigInteger.valueOf(150).toByteArray());
+                transaction.set("konto_B".getBytes(), BigInteger.valueOf(100).toByteArray());
                 return null;
             });
 
-            String value = db.run(transaction -> {
-                byte[] result = transaction.get("my_key".getBytes()).join();
-                return new String(result);
+            db.run(transaction -> {
+                BigInteger kontoA = new BigInteger(transaction.get("konto_A".getBytes()).join());
+                BigInteger kontoB = new BigInteger(transaction.get("konto_B".getBytes()).join());
+                kontoA = kontoA.subtract(BigInteger.valueOf(90));
+                kontoB = kontoB.add(BigInteger.valueOf(90));
+                transaction.set("konto_A".getBytes(), kontoA.toByteArray());
+                transaction.set("konto_B".getBytes(), kontoB.toByteArray());
+                return null;
             });
 
-            System.out.println(value);
+            db.run(transaction -> {
+                BigInteger kontoA = new BigInteger(transaction.get("konto_A".getBytes()).join());
+                BigInteger kontoB = new BigInteger(transaction.get("konto_B".getBytes()).join());
+                System.out.println("kontoA: " + kontoA.toString());
+                System.out.println("kontoB: " + kontoB.toString());
+                return null;
+            });
         }
     }
 }
+
